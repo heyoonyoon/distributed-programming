b85bc1bdc38ae22b7ece2ca5c7d87e111391677a
heyoonyoon <heeyoon1302@naver.com>
2026-06-04 06:22:32 +0900

    Epic4: 보상심사 확정 요청에 payoutAmount 추가 + 컨트롤러 전달

--- backend/src/main/java/com/distribution/insurance/web/dto/ConfirmBenefitReviewRequest.java
+++ backend/src/main/java/com/distribution/insurance/web/dto/ConfirmBenefitReviewRequest.java
@@ -3,4 +3,4 @@ package com.distribution.insurance.web.dto;
 import com.distribution.insurance.domain.review.ReviewResult;
 import jakarta.validation.constraints.NotNull;
 
-public record ConfirmBenefitReviewRequest(@NotNull ReviewResult result, String comment) {}
+public record ConfirmBenefitReviewRequest(@NotNull ReviewResult result, String comment, Integer payoutAmount) {}
